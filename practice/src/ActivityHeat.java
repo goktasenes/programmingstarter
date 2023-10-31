fa529d31f2cc6e8c626bdddebb1674613af5f4b4
goktasenes <enesgoktas42@gmail.com>
2023-10-31 14:00:06 -0400

    initial commit

---
+++ practice/src/ActivityHeat.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ActivityHeat {
+
+	public static void main(String[] args) {
+				
+		Scanner inp = new Scanner(System.in);
+		
+		System.out.println("Enter the temprature: ");
+		int heat = inp.nextInt();
+		
+		if (heat<5) {
+			System.out.println("Try skiing");
+		}
+		
+		else if (heat >= 5 && heat <= 15) {
+			System.out.println("Try cinema");
+			
+		}
+		
+		else if (heat > 15 && heat <= 25) {
+			System.out.println("Have a picnic");
+			
+		}
+		
+		else{
+			System.out.println("Try swimming");
+		}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
